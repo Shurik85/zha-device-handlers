7edda7e155e1a9ce0151e2e3108d5ea77d6ce1e6
Shurik85 <78793454+Shurik85@users.noreply.github.com>
2022-06-18 15:43:21 +0300

    Update __init__.py

--- zhaquirks/xiaomi/aqara/__init__.py
+++ zhaquirks/xiaomi/aqara/__init__.py
@@ -1,1 +1,26 @@
 """Module for Xiaomi Aqara quirks implementations."""
+from zigpy import types
+from zigpy.zcl.clusters.general import MultistateInput, OnOff
+
+from .. import BasicCluster
+from ... import EventableCluster
+
+
+class BasicClusterDecoupled(BasicCluster):
+    """Adds attributes for decoupled mode."""
+
+    # Known Options for 'decoupled_mode_<button>':
+    # * 254 (decoupled)
+    # * 18 (relay controlled)
+    manufacturer_attributes = {
+        0xFF22: ("decoupled_mode_left", types.uint8_t),
+        0xFF23: ("decoupled_mode_right", types.uint8_t),
+    }
+
+
+class WallSwitchMultistateInputCluster(EventableCluster, MultistateInput):
+    """WallSwitchMultistateInputCluster: fire events corresponding to press type."""
+
+
+class WallSwitchOnOffCluster(EventableCluster, OnOff):
+    """WallSwitchOnOffCluster: fire events corresponding to press type."""
